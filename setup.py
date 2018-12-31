1b6b0e71b64668769778e011f2d49d46e0bbc13b
hungluongkong <46273584+hungluongkong@users.noreply.github.com>
2018-12-31 11:20:23 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author_email='jbillaud@gmail.com',
     packages=['debug_panel'],
     include_package_data=True,
-    url='https://github.com/recamshak/django-debug-panel',
+    url='https://github.com/hungluongkong/django2.0-debug-panel',
     license='BSD',
     description='django-debug-toolbar in WebKit DevTools. Works fine with background Ajax requests and non-HTML responses',
     long_description=open('README.rst').read(),
